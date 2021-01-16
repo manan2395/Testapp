f23a7d795940faa566b38f8e8c71e200f60b00da
manan2395 <manan@maxput.com>
2021-01-16 15:43:19 +0530

    Update

--- app.php
+++ app.php
@@ -1,5 +1,13 @@
-$data =  "Manan Suthar";
-echo $data;
-//this is new application
+<?php
+	$data =  "Manan Suthar";
+        echo $data;
+	//this is new application	
+	$i=5;
+	for($i=0;$i<5;$i++)
+	{
+		echo "Manan Suthar".$i;
+		echo "<br>";		
+	}
+?>
 
 
